2fbeff58c2c5bccc04e2d13e59a6b722b87a97c0
chailyuan <chailyuan2@gmail.com>
2017-08-08 14:48:15 +0800

    添加两种算法

--- FindArrayMax/main.cpp
+++ FindArrayMax/main.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int array[10]={32,432,67,98,035,86,97,234,23,567};
+int array[20]={32,432,67,98,035,86,97,234,23,567,32,432,67,98,035,86,97,234,23,567};
 int nnn = 0;
 
 int findMax(int* src,int length){
 	nnn++;
 	if (length==0) return 0;
 	if (length==1) return src[0];
-//	if (length==2) return src[0]>src[1]?src[0]:src[1];
+	int wen = findMax(src,(length+1)/2);
+	int longfei = findMax(src+(length+1)/2,(length+1)/2);
+	return wen>longfei?
+		wen:longfei;
+}
 
-	return findMax(src,(length+1)/2)>findMax(src+(length+1)/2,(length+1)/2)?
-		findMax(src,(length+1)/2):findMax(src+(length+1)/2,(length+1)/2);
+int findMax2(int *src,int length){
+	nnn++;
+	if (length==0) return 0;
+	int wenlongfei = findMax2(src+1,length-1);
+	return src[0]>wenlongfei?
+		src[0]:wenlongfei;
 }
 
 int main(){
-	int a = findMax(array,10);
+	int a = findMax(array,20);
 	printf("%d\n",a);
 	printf("%d\n",nnn);
 	system("pause");
